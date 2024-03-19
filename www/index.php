790e242afcb9fd4b2133012429249f8cc0412ad8
LeZwart <192443@novacollege.nl>
2024-03-19 13:51:41 +0100

    day2commit

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>NOVA Restaurant</title>
+    </head>
+    <body>
+        <main class="main-page-index">
+            <section class="section-index" id="text-section">
+                <h1>Welkom bij het Oostenrijkse NOVA Restaurant</h1>
+                <p>Een heerlijke en gezellige avond beleef je bij ons!</p>
+                <img src="/www/img/WienerSchnitzel.jpg" alt="Wiener Schnitzel">
+            </section>
+            <section class="section-index" id="button-section">
+                <a href="account.php" class="main-page-button">Account aanmaken</a>
+                <a href="#" class="main-page-button">Inloggen</a>
+                <a href="menu.php" class="main-page-button">Menu</a>
+            </section>
+        </main>
+    </body>
+</html>
---
+++ www/php/header.php
@@ -0,0 +1,9 @@
+<Header>
+    <nav>
+        <ul>
+            <li><a class="nav-button" href="index.php">Home</a></li>
+            <li><a class="nav-button" href="#">Menu</a></li>
+            <li><a class="nav-button" href="#">Account aanmaken</a></li>
+        </ul>
+    </nav>
+</Header>
