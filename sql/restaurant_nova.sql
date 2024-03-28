6e66d89507917d8f64828e15521411da46272e69
LeZwart <192443@novacollege.nl>
2024-03-28 10:18:07 +0100

    Updated restaurant_nova.sql :)

--- sql/restaurant_nova.sql
+++ sql/restaurant_nova.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Mar 22, 2024 at 08:13 AM
+-- Generation Time: Mar 28, 2024 at 09:17 AM
 -- Server version: 10.4.32-MariaDB-1:10.4.32+maria~ubu2004
 -- PHP Version: 8.2.17
 
@@ -40,7 +40,10 @@ CREATE TABLE `adres` (
 --
 
 INSERT INTO `adres` (`adresid`, `postcode`, `huisnummer`, `woonplaats`, `straatnaam`) VALUES
-(12, '2051 JA', 29, 'Overveen', 'Prinshendriklaan');
+(12, '2051 JA', 29, 'Overveen', 'Prinshendriklaan'),
+(18, '2222 RE', 32, 'Leiden', 'weet ik niet'),
+(19, '1234QW', 12, 'qwert', 'qwerty'),
+(20, '2201 DU', 11, 'Daghove', 'Dagostraat');
 
 -- --------------------------------------------------------
 
@@ -63,7 +66,10 @@ CREATE TABLE `gebruiker` (
 --
 
 INSERT INTO `gebruiker` (`gebruikerid`, `adresid`, `voornaam`, `achternaam`, `wachtwoord`, `email`, `rol`) VALUES
-(7, 12, 'Mickey', 'Mouse', '$2y$10$ymnPH3Rx8.3nv4atwQp/W.JKZ90naOQsWdEMpqzFfi/GX4BzmtDRO', '192443@novacollege.nl', 'Directeur');
+(7, 12, 'Mickey', 'Mouse', '$2y$10$ymnPH3Rx8.3nv4atwQp/W.JKZ90naOQsWdEMpqzFfi/GX4BzmtDRO', '192443@novacollege.nl', 'Directeur'),
+(13, 18, 'piet', 'paaltjens', '$2y$10$6UxCs7Gmgbw.q5ojozsPGeKo6qlq.U/8ColgYdaoZsKyoYc2OiYv.', 'gebruiker@spaghettificatie.nl', 'Klant'),
+(14, 19, 'qwerty', 'uiop', '$2y$10$PvY07an1elv08cnu.C.WJeJQe4YwF8f89rMg.5r5IqJg0TTybs4ke', 'admin@work4me.nl', 'Klant'),
+(15, 20, 'Donald', 'Duck', '$2y$10$GXXP8OA1F9FGlFL5Yq9ThurAm0Tfbb4WLM6Y14i7T15k5CAqNFLMS', 'donald@duck.be', 'Klant');
 
 -- --------------------------------------------------------
 
@@ -75,6 +81,16 @@ CREATE TABLE `menugang` (
   `menugangid` int(11) NOT NULL,
   `naam` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `menugang`
+--
+
+INSERT INTO `menugang` (`menugangid`, `naam`) VALUES
+(1, 'Wiener Schnitzel'),
+(2, 'Geschnetzeltes'),
+(3, 'Wijn'),
+(4, 'Drinken');
 
 -- --------------------------------------------------------
 
@@ -95,6 +111,18 @@ CREATE TABLE `product` (
   `aantal_voorraad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productid`, `categorieid`, `menugangid`, `naam`, `beschrijving`, `inkoopprijs`, `verkoopprijs`, `afbeelding`, `is_vega`, `aantal_voorraad`) VALUES
+(1, 1, 1, 'Tomatensoep', 'Een geroosterde-tomatensoep met basilicum en kaascroutons', '1025', '2000', '', 1, 21),
+(2, 2, 1, 'Wiener Schnitzel', 'Geserveerd met aardappelsalade en een frisse groene salade met vinaigrette dressing', '1500', '2500', '', 0, 11),
+(3, 3, 1, 'Apfelstrudel', 'Deze apfelstrudel wordt geserveerd met vanillesaus. Apfelstrudel bestaat uit deeg, plakjes appel en rozijnen.', '800', '1000', '', 1, 15),
+(4, 1, 2, 'Käsespätzle', 'Zachte, huisgemaakte noedels vermengd met gesmolten kaas, gebakken ui en geserveerd met een frisse salade.', '900', '1000', '', 1, 11),
+(5, 2, 2, 'Geschnetzeltes', 'malse reepjes varkensvlees of kalfsvlees, gebakken met champignons en ui in een romige saus, geserveerd met knapperige Rösti-aardappelen en gestoomde groenten.', '1100', '1500', '', 0, 12),
+(6, 3, 2, 'Sachertorte', 'een weelderige chocoladetaart gevuld met abrikozenjam en bedekt met een glanzende laag pure chocolade, geserveerd met een toefje slagroom en een kopje sterke Oostenrijkse koffie.', '800', '1200', '', 1, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -105,6 +133,15 @@ CREATE TABLE `product_categorie` (
   `categorieid` int(11) NOT NULL,
   `categorie` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `product_categorie`
+--
+
+INSERT INTO `product_categorie` (`categorieid`, `categorie`) VALUES
+(1, 'Voorgerecht'),
+(2, 'Hoofdgerecht'),
+(3, 'Desert');
 
 -- --------------------------------------------------------
 
@@ -163,7 +200,8 @@ ALTER TABLE `product_categorie`
 --
 ALTER TABLE `reservering`
   ADD PRIMARY KEY (`reserveringid`),
-  ADD KEY `gebruikerid` (`gebruikerid`);
+  ADD KEY `gebruikerid` (`gebruikerid`),
+  ADD KEY `tafelnummer` (`tafelnummer`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -173,31 +211,31 @@ ALTER TABLE `reservering`
 -- AUTO_INCREMENT for table `adres`
 --
 ALTER TABLE `adres`
-  MODIFY `adresid` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `adresid` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `gebruiker`
 --
 ALTER TABLE `gebruiker`
-  MODIFY `gebruikerid` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `gebruikerid` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `menugang`
 --
 ALTER TABLE `menugang`
-  MODIFY `menugangid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `menugangid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `product_categorie`
 --
 ALTER TABLE `product_categorie`
-  MODIFY `categorieid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `categorieid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reservering`
